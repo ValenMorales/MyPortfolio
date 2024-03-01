2c51161ccf27561614f8ba4c0f7fd1a9d6bb7341
ValenMorales <valentina.moraless@autonoma.edu.co>
2024-03-01 11:41:31 -0500

    projects page

--- js/projects.js
+++ js/projects.js
@@ -1,41 +1,45 @@
-let scrollContainer = document.querySelector(".gallery");
-let backBtn = document.getElementById("backBtn");
-let nextBtn = document.getElementById("nextBtn");
-
-scrollContainer.addEventListener("wheel", (evt) => {
-    evt.preventDefault();
-    scrollContainer.scrollLeft += evt.deltaY;
-});
-
-nextBtn.addEventListener("click", () => {
-    scrollContainer.style.scrollBehavior = "smooth";
-    scrollContainer.scrollLeft += 900;
-});
-backBtn.addEventListener("click", () => {
-    scrollContainer.style.scrollBehavior = "smooth";
-    scrollContainer.scrollLeft -= 900;
-}   );
-
-
 document.addEventListener("DOMContentLoaded", function() {
+    var galleryContainers = document.querySelectorAll('.gallery-wrap');
+
+    galleryContainers.forEach(function(container) {
+        var scrollContainer = container.querySelector(".gallery");
+        var backBtn = container.querySelector(".backBtn");
+        var nextBtn = container.querySelector(".nextBtn");
+
+        scrollContainer.addEventListener("wheel", (evt) => {
+            evt.preventDefault();
+            scrollContainer.scrollLeft += evt.deltaY;
+        });
+
+        nextBtn.addEventListener("click", () => {
+            scrollContainer.style.scrollBehavior = "smooth";
+            scrollContainer.scrollLeft += 900;
+        });
+
+        backBtn.addEventListener("click", () => {
+            scrollContainer.style.scrollBehavior = "smooth";
+            scrollContainer.scrollLeft -= 900;
+        });
+    });
+
     var propertyElements = document.querySelectorAll('.project');
 
     function isInViewport(element) {
-      var rect = element.getBoundingClientRect();
-      return (
-        rect.top >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-      );
+        var rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+        );
     }
 
     function activarAparecer() {
-      propertyElements.forEach(function(element) {
-     
-        if (isInViewport(element)) {
-          element.classList.add('aparecer1 2s ease 1 both');
-        }
-      });
+        propertyElements.forEach(function(element) {
+            if (isInViewport(element)) {
+                element.classList.add('aparecer1', '2s', 'ease', '1', 'both');
+            }
+        });
     }
+
     activarAparecer();
     window.addEventListener('scroll', activarAparecer);
-  });
+});
